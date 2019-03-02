d94f470462639566a76146a016021fd806f742d5
radiantdinosaurs <radiantdinosaurs@gmail.com>
2019-03-01 23:48:23 -0800

    Changes in file structure

--- app/app.js
+++ app/app.js
@@ -1,26 +1,37 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const router = require('./routes');
-const logger = require('./logger/index');
-const error = require('./error/index');
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = require("./routes");
+const logger = require("./logger/index");
+const error = require("./error/index");
 
 const app = express();
 
 // config ===============================
-app.set('port', 8080);
-app.use(bodyParser.json({ limit: '50mb' }));
+app.set("port", process.env.PORT || 8080);
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(
     bodyParser.urlencoded({
-        limit: '50mb',
+        limit: "50mb",
         extended: true,
         parameterLimit: 50000
     })
 );
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/client/build")));
+    app.get("*", (request, response) => {
+        res.sendfile(path.join((__dirname = "/client/build/index.html")));
+    });
+}
+
+app.get("*", (request, response) => {
+    res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
+
 // routes ===============================
-app.use('/', router);
+app.use("/", router);
 
 // handles when route is not found
 app.use((request, response, next) => next(error.resourceNotFound));
@@ -29,29 +40,29 @@ app.use((request, response, next) => next(error.resourceNotFound));
 app.use((error, request, response, next) => {
     if (error) {
         if (error.code) {
-            logger.log('error', error);
+            logger.log("error", error);
             response.status(error.code).send({
                 status: error.code,
                 error: error.message
             });
         } else {
-            logger.log('error', error);
+            logger.log("error", error);
             response.status(500).send({
                 status: 500,
-                error: 'Unexpected error.'
+                error: "Unexpected error."
             });
         }
     }
 });
 
 // launch ===============================
-app.listen(app.get('port'), error => {
-    if (error) logger.log('error', error);
+app.listen(app.get("port"), error => {
+    if (error) logger.log("error", error);
     else {
         logger.log({
-            level: 'info',
+            level: "info",
             message:
-                'App is running, server is listening on port ' + app.get('port')
+                "App is running, server is listening on port " + app.get("port")
         });
     }
 });
